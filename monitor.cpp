64040ae3f551f3f80187a9476071f7b0ec0412b9
AjayB17 <abajstyles@gmail.com>
2025-08-11 16:33:04 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -12,22 +12,6 @@ constexpr float SPO2_MIN = 90.0f;
 constexpr int OK = 1;
 constexpr int NOT_OK = 0;
 
-int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > TEMP_HIGH || temperature < TEMP_LOW) {
-    cout << "Temperature is critical!\n";
-    blinkAlert();
-    return NOT_OK;
-  } else if (pulseRate < PULSE_LOW || pulseRate > PULSE_HIGH) {
-    cout << "Pulse Rate is out of range!\n";
-    blinkAlert();
-    return NOT_OK;
-  } else if (spo2 < SPO2_MIN) {
-    cout << "Oxygen Saturation out of range!\n";
-    blinkAlert();
-    return NOT_OK;
-  }
-  return OK;
-}
 void blinkAlert() {
   for (int i = 0; i < 6; i++) {
     cout << "\r* " << flush;
@@ -37,3 +21,33 @@ void blinkAlert() {
   }
   cout << "\r  \r" << flush; // Clear line after alert
 }
+
+int tempOk(float temperature){
+    if (temperature > TEMP_HIGH || temperature < TEMP_LOW) {
+    cout << "Temperature is critical!\n";
+    blinkAlert();
+    return NOT_OK;
+    }
+  return OK;
+}
+int pulseRateOk(float pulseRate){
+  if (pulseRate < PULSE_LOW || pulseRate > PULSE_HIGH) {
+    cout << "Pulse Rate is out of range!\n";
+    blinkAlert();
+    return NOT_OK;
+  }
+return OK;
+}
+int spo2Ok(float spo2){
+  if (spo2 < SPO2_MIN) {
+    cout << "Oxygen Saturation out of range!\n";
+    blinkAlert();
+    return NOT_OK;
+  }
+return OK;
+}
+
+int vitalsOk(float temperature, float pulseRate, float spo2) {
+  return tempOk(temperature) && pulseRateOk(pulseRate) && spo2Ok(spo2);
+}
+
