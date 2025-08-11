27d2fbc25d3cbfdb56da98bbdf59534cfdb832c6
AjayB17 <abajstyles@gmail.com>
2025-08-11 16:19:03 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -4,35 +4,36 @@
 #include <chrono>
 #include <iostream>
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
+constexpr float TEMP_LOW = 95.0f;
+constexpr float TEMP_HIGH = 102.0f;
+constexpr float PULSE_LOW = 60.0f;
+constexpr float PULSE_HIGH = 100.0f;
+constexpr float SPO2_MIN = 90.0f;
+constexpr int OK = 1;
+constexpr int NOT_OK = 0;
 
 int vitalsOk(float temperature, float pulseRate, float spo2) {
-  if (temperature > 102 || temperature < 95) {
+  if (temperature > TEMP_HIGH || temperature < TEMP_LOW) {
     cout << "Temperature is critical!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (pulseRate < 60 || pulseRate > 100) {
+    blinkAlert();
+    return NOT_OK;
+  } else if (pulseRate < PULSE_LOW || pulseRate > PULSE_HIGH) {
     cout << "Pulse Rate is out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
-  } else if (spo2 < 90) {
+    blinkAlert();
+    return NOT_OK;
+  } else if (spo2 < SPO2_MIN) {
     cout << "Oxygen Saturation out of range!\n";
-    for (int i = 0; i < 6; i++) {
-      cout << "\r* " << flush;
-      sleep_for(seconds(1));
-      cout << "\r *" << flush;
-      sleep_for(seconds(1));
-    }
-    return 0;
+    blinkAlert();
+    return NOT_OK;
   }
-  return 1;
+  return OK;
+}
+void blinkAlert() {
+  for (int i = 0; i < 6; i++) {
+    cout << "\r* " << flush;
+    sleep_for(seconds(1));
+    cout << "\r *" << flush;
+    sleep_for(seconds(1));
+  }
+  cout << "\r  \r" << flush; // Clear line after alert
 }
